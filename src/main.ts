170bbd3fedf0702a3d82507d6de38d870e981625
Ping <muhammadyusufa2111@gmail.com>
2025-12-17 05:55:53 +0500

    fixed cors

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:3000'],
+    origin: ['http://localhost:3000', 'https://www.romitan-barbershop.uz'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
