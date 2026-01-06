d33c553b8164af8b90ab5f3e31ef0871a246118b
proxy <muhammadyusufa2111@gmail.com>
2026-01-06 18:15:11 +0500

    added console log to display application URL on startup

--- src/main.ts
+++ src/main.ts
@@ -32,5 +32,7 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
 
   await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
+
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
