b1a790db5d72d77aaee496ed8778da7c4137251d
proxy <muhammadyusufa2111@gmail.com>
2026-01-07 18:15:27 +0500

    commented out verify method

--- src/telegram/telegram.controller.ts
+++ src/telegram/telegram.controller.ts
@@ -1,6 +1,5 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Controller  } from '@nestjs/common';
 import { TelegramService } from './telegram.service';
-import { TelegramInitDataDto } from './dto/telegram-init-data.dto';
 
 export interface TelegramUser {
   id: number;
@@ -24,31 +23,31 @@ export interface VerifiedTelegramData {
 export class TelegramController {
   constructor(private readonly telegramService: TelegramService) {}
 
-  @Post('verify')
-  verify(@Body() dto: TelegramInitDataDto) {
-    const botToken = process.env.TELEGRAM_BOT_TOKEN;
-
-    if (!botToken) {
-      throw new Error('BOT_TOKEN not configured');
-    }
-
-    // Вызываем метод сервиса (который мы типизировали ранее)
-    const rawData = this.telegramService.verifyTelegramInitData(
-      dto.initData,
-      botToken,
-    );
-
-    // Парсим поле user, так как Telegram передает его строкой JSON
-    let userData: TelegramUser | undefined;
-    if (rawData.user) {
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-      userData = JSON.parse(rawData.user as unknown as string);
-    }
-
-    return {
-      message: 'Valid Telegram user',
-      userData,
-      authDate: rawData.auth_date,
-    };
-  }
+  // @Post('verify')
+  // verify(@Body() dto: TelegramInitDataDto) {
+  //   const botToken = process.env.TELEGRAM_BOT_TOKEN;
+  //
+  //   if (!botToken) {
+  //     throw new Error('BOT_TOKEN not configured');
+  //   }
+  //
+  //   // Вызываем метод сервиса (который мы типизировали ранее)
+  //   const rawData = this.telegramService.verifyTelegramInitData(
+  //     dto.initData,
+  //     botToken,
+  //   );
+  //
+  //   // Парсим поле user, так как Telegram передает его строкой JSON
+  //   let userData: TelegramUser | undefined;
+  //   if (rawData.user) {
+  //     // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  //     userData = JSON.parse(rawData.user as unknown as string);
+  //   }
+  //
+  //   return {
+  //     message: 'Valid Telegram user',
+  //     userData,
+  //     authDate: rawData.auth_date,
+  //   };
+  // }
 }
